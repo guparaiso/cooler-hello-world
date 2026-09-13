f68f2ff9ff39609ded2aa66674f5802fe2992f3e
gustavo <guparaiso333@gmail.com>
2026-09-12 22:28:30 -0300

    Adiciona efeito no print hello world

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        String letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !";
+        String certo = "Hello World!";
+        String atual = "";
+
+        while (!atual.equals(certo)) {
+            for (int i = 0; i < letras.length(); i++) {
+                char letra = letras.charAt(i);
+                System.out.println(atual + letra);
+                Thread.sleep(50);
+
+                int posicaoAtual = atual.length();
+                char letraCerta = certo.charAt(posicaoAtual);
+
+                if (letra == letraCerta) {
+                    atual += letra;
+                    break;
+                }
+            }
+        }
 
     }
 }
